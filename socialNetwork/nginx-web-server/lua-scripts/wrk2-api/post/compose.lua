9c04000a9a682d89e7da0e83ead08044f599c75a
Vaastav Anand <vaastav.anand05@gmail.com>
2019-05-28 15:19:00 +0200

    Add instrumentation for nginx lua compose script

--- socialNetwork/nginx-web-server/lua-scripts/wrk2-api/post/compose.lua
+++ socialNetwork/nginx-web-server/lua-scripts/wrk2-api/post/compose.lua
@@ -1,64 +1,81 @@
 local _M = {}
+local xtracer = require "luaxtrace"
 
 local function _StrIsEmpty(s)
   return s == nil or s == ''
 end
 
 local function _UploadUserId(req_id, post, carrier, baggage)
+  xtracer.SetBaggage(baggage)
   local GenericObjectPool = require "GenericObjectPool"
   local UserServiceClient = require "social_network_UserService"
   local ngx = ngx
 
   local user_client = GenericObjectPool:connection(
       UserServiceClient, "user-service", 9090)
+  carrier["baggage"] = baggage
   local status, err = pcall(user_client.UploadCreatorWithUserId, user_client,
       req_id, tonumber(post.user_id), post.username, carrier)
   if not status then
     ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
     ngx.say("Upload user_id failed: " .. err.message)
     ngx.log(ngx.ERR, "Upload user_id failed: " .. err.message)
+    xtracer.LogXTrace("Upload user_id failed " .. err.message)
+    xtracer.DeleteBaggage()
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
   GenericObjectPool:returnConnection(user_client)
+  xtracer.DeleteBaggage()
 end
 
 local function _UploadText(req_id, post, carrier, baggage)
+  xtracer.SetBaggage(baggage)
   local GenericObjectPool = require "GenericObjectPool"
   local TextServiceClient = require "social_network_TextService"
   local ngx = ngx
 
   local text_client = GenericObjectPool:connection(
       TextServiceClient, "text-service", 9090)
+  carrier["baggage"] = baggage
   local status, err = pcall(text_client.UploadText, text_client, req_id,
       post.text, carrier)
   if not status then
     ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
     ngx.say("Upload text failed: " .. err.message)
     ngx.log(ngx.ERR, "Upload text failed: " .. err.message)
+    xtracer.LogXTrace("Upload text failed " .. err.message)
+    xtracer.DeleteBaggage()
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
   GenericObjectPool:returnConnection(text_client)
+  xtracer.DeleteBaggage()
 end
 
 local function _UploadUniqueId(req_id, post, carrier, baggage)
+  xtracer.SetBaggage(baggage)
   local GenericObjectPool = require "GenericObjectPool"
   local UniqueIdServiceClient = require "social_network_UniqueIdService"
   local ngx = ngx
 
   local unique_id_client = GenericObjectPool:connection(
       UniqueIdServiceClient, "unique-id-service", 9090)
+  carrier["baggage"] = baggage
   local status, err = pcall(unique_id_client.UploadUniqueId, unique_id_client,
       req_id, tonumber(post.post_type), carrier)
   if not status then
     ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
     ngx.say("Upload unique_id failed: " .. err.message)
     ngx.log(ngx.ERR, "Upload unique_id failed: " .. err.message)
+    xtracer.LogXTrace("Upload unique_id failed " .. err.message)
+    xtracer.DeleteBaggage()
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
   GenericObjectPool:returnConnection(unique_id_client)
+  xtracer.DeleteBaggage()
 end
 
 local function _UploadMedia(req_id, post, carrier, baggage)
+  xtracer.SetBaggage(baggage)
   local GenericObjectPool = require "GenericObjectPool"
   local MediaServiceClient = require "social_network_MediaService"
   local cjson = require "cjson"
@@ -67,6 +84,7 @@ local function _UploadMedia(req_id, post, carrier, baggage)
   local media_client = GenericObjectPool:connection(
       MediaServiceClient, "media-service", 9090)
   local status, err
+  carrier["baggage"] = baggage
   if (not _StrIsEmpty(post.media_ids) and not _StrIsEmpty(post.media_types)) then
     status, err = pcall(media_client.UploadMedia, media_client,
         req_id, cjson.decode(post.media_types), cjson.decode(post.media_ids), carrier)
@@ -78,9 +96,12 @@ local function _UploadMedia(req_id, post, carrier, baggage)
     ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
     ngx.say("Upload media failed: " .. err.message)
     ngx.log(ngx.ERR, "Upload media failed: " .. err.message)
+    xtracer.LogXTrace("Upload media failed " .. err.message)
+    xtracer.DeleteBaggage()
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
   GenericObjectPool:returnConnection(media_client)
+  xtracer.DeleteBaggage()
 end
 
 function _M.ComposePost()
@@ -88,7 +109,6 @@ function _M.ComposePost()
   local ngx = ngx
   local cjson = require "cjson"
   local jwt = require "resty.jwt"
-  local xtracer = require "luaxtrace"
 
   local tracing = xtracer.IsTracing()
   if tracing ~= true then
