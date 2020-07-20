c67508a34a7c91c9509c091b5f3b3e772c6edca5
João Ferreira Loff <jfloff@gmail.com>
2020-07-20 18:38:47 +0000

    Add TBB sample

--- socialNetwork/src/AntipodeOracle/AntipodeOracle.h
+++ socialNetwork/src/AntipodeOracle/AntipodeOracle.h
@@ -24,6 +24,9 @@
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 
+#include <tbb/tbb.h>
+#include <tbb/concurrent_unordered_set.h>
+
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
@@ -33,6 +36,8 @@ namespace social_network {
 
   class AntipodeOracleHandler : virtual public AntipodeOracleIf {
   public:
+    tbb::concurrent_unordered_set<int64_t, tbb::tbb_hash<int64_t>, std::equal_to<int>> cache;
+
     AntipodeOracleHandler() {
       // Your initialization goes here
     }
@@ -40,11 +45,21 @@ namespace social_network {
     bool MakeVisible(const int64_t object_id) {
       // Your implementation goes here
       LOG(error) << "MakeVisible: " << object_id;
+      cache.insert(object_id);
+
+      tbb::concurrent_unordered_set<int64_t, tbb::tbb_hash<int64_t>, std::equal_to<int>>::iterator cacheit;
+      cacheit = cache.begin();
+      while (cacheit != cache.end()){
+        LOG(error) << "cache: " << *cacheit;
+        ++cacheit;
+      }
+      return 0;
     }
 
     bool IsVisible(const int64_t object_id) {
       // Your implementation goes here
       LOG(error) << "IsVisible: " << object_id;
+      cache.insert(object_id);
     }
   };
 
